0665b1b82d18106167369ad85783be565a1b9ad3
staidigital <sondre.stai@gmail.com>
2024-12-02 17:28:27 +0100

    fix

--- 2/test.py
+++
@@ -1,8 +0,0 @@
-l = [1,2,3,4]
-unsafe_counter = 0
-for i, num in enumerate(l):
-        if i < len(l)-1:
-            if not l[i] < min(l[i+1:]) or not l[i] > max(l[i+1:]):
-                unsafe_counter += 1
-
-print(unsafe_counter)
